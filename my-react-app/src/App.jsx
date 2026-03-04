dedfccc8562eaca8bac7dc6c4c92ef0830b0a05d
Ayman Azzaoui <ayman.azzaoui@MacBook-Pro-van-Ayman.local>
2026-03-04 15:46:31 +0100

    Veel gewerkt aan react demo project en Git

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import Footer from './Footer.jsx';
 import Food from './Food.jsx';
 import Card from './Card.jsx';
 import Button from './Button.jsx';
+import Student from './Student.jsx';
 
 function App() {
   return (
@@ -12,6 +13,13 @@ function App() {
       <Footer></Footer>
       <Card></Card>
       <Button></Button>
+      <Student></Student>
+
+      
+      <Student name="John Doe" age={20} isStudent={true} />
+      <Student name="Jane Smith" age={25} isStudent={false} />
+      
+
       
 
     </div>
--- my-react-app/src/Button.jsx
+++ my-react-app/src/Button.jsx
@@ -1,7 +1,21 @@
+
+
 function Button () {
 
+    const styles = {
+        btn: {
+            backgroundColor: 'blue',
+            color: "white",
+            padding: "10px 20px",
+            border: "none",
+            borderRadius: "5px",
+            cursor: "pointer"
+        }
+    }
+
     return(
-<button className="btn">Click me</button>
+<button className={styles.btn}>Click me</button>
+
     );
 
 }
