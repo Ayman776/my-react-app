b3167e723c5b3dad1017c11b8810ce2503b8b819
Ayman Azzaoui <ayman.azzaoui@MacBook-Pro-van-Ayman.local>
2026-03-25 09:36:37 +0100

    updated

--- my-react-app/src/Header.jsx
+++ my-react-app/src/Header.jsx
@@ -16,6 +16,7 @@ function Header() {
                 </div>
 
 
+
                 <div className="header-right">
                     <div className="notification">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" id="Bell-Notification--Streamline-Sharp" height="24" width="24">
--- my-react-app/src/pages/Profile.jsx
+++ my-react-app/src/pages/Profile.jsx
@@ -1,32 +1,64 @@
+import { useState } from "react";
+
 function Profile() {
-  const user = {
-    name: "Ayman A",
-    email: "ayman@email.com",
-    joined: "2024",
+  const [name, setName] = useState("Ayman A");
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleSave = () => {
+    setIsEditing(false);
+    console.log("Nieuwe naam:", name);
   };
 
   return (
     <div className="profile-page">
-
       <h1>Profiel</h1>
 
       <div className="profile-container">
 
-        {/* PROFIEL INFO */}
         <div className="profile-card">
           <h3>Gebruiker</h3>
 
-          <p><strong>Naam:</strong> {user.name}</p>
-          <p><strong>Email:</strong> {user.email}</p>
-          <p><strong>Lid sinds:</strong> {user.joined}</p>
+         
+          <div className="profile-field">
+            <strong>Naam:</strong>
+
+            {isEditing ? (
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="profile-input"
+              />
+            ) : (
+              <span className="profile-value">{name}</span>
+            )}
+
+            {!isEditing ? (
+              <button
+                className="edit-btn"
+                onClick={() => setIsEditing(true)}
+              >
+                Bewerken
+              </button>
+            ) : (
+              <button
+                className="save-btn"
+                onClick={handleSave}
+              >
+                Opslaan
+              </button>
+            )}
+          </div>
+
+          <p><strong>Email:</strong> ayman@email.com</p>
+          <p><strong>Lid sinds:</strong> 2024</p>
         </div>
 
-        {/* INSTELLINGEN */}
         <div className="profile-card">
           <h3>Instellingen</h3>
 
           <button className="profile-btn">Wachtwoord wijzigen</button>
-          <button className="profile-btn">Account verwijderen</button>
+          <button className="profile-btnn">Account verwijderen</button>
         </div>
 
       </div>
